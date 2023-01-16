9941b09f1e5337a969347cc04aea6c69a2fdaab6
khanhkhanh196 <48844793+khanhkhanh196@users.noreply.github.com>
2023-01-16 10:33:36 +0700

    update

---
+++ src/main/java/com/example/demo/properties/FileStorageProperties.java
@@ -0,0 +1,11 @@
+package com.example.demo.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "file")
+@Data
+public class FileStorageProperties {
+    private String uploadDir;
+
+}
