46bf4e7806614e8cb75cdc4b1a2f6b57e2919fb5
Dung <michealng385@gmail.com>
2023-01-17 13:35:44 +0700

    Controller unit test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories
+//@EnableJpaRepositories
 public class DemoApplication {
 
 	public static void main(String[] args) {
